57013600ca5dfc48aad43a113ef38e3dca16a5e6
arntgm <agmalvik@gmail.com>
2017-03-14 20:54:11 +0100

    fastNonDominatedSort

--- src/MasterEA.java
+++ src/MasterEA.java
@@ -20,6 +20,7 @@ public class MasterEA {
 	}
 	
 	private void init() {
+		objectives = new String[] {"devi", "edge", "conn"};
 		//newColor = fh.getPixels();
 		//run();
 	}
@@ -33,26 +34,92 @@ public class MasterEA {
 		fh.saveNewImage(newColor);
 	}
 	
-	private void crowdingDistanceAssignment(ArrayList<Chromosome> F) {
+	private List<List<Chromosome>> fastNonDominatedSort(List<Chromosome> pop) {
+		int dom;
+		List<List<Chromosome>> F = new ArrayList<List<Chromosome>>();
+		List<Chromosome> F1 = new ArrayList<Chromosome>();
+		for (Chromosome p : pop) {
+			p.dominationList = new ArrayList<Chromosome>();
+			p.dominatedByCounter = 0;
+			for (Chromosome q : pop) {
+				dom = dominates(p,q);
+				if (dom == 3) {
+					p.dominationList.add(q);
+				} else if (dom == -3) {
+					p.dominatedByCounter += 1;
+				}
+			}
+			if (p.dominatedByCounter == 0) {
+				p.nonDomRank = 1;
+				F1.add(p);
+			}
+		}
+		F.add(F1);
+		int i = 0;
+		List<Chromosome> Q;
+		while (! F.get(i).isEmpty()) {
+			Q = new ArrayList<Chromosome>();
+			for (Chromosome p : F.get(i)) {
+				for (Chromosome q : p.dominationList) {
+					q.dominatedByCounter -= 1;
+					if (q.dominatedByCounter == 0) {
+						q.nonDomRank = i+2;
+						Q.add(q);
+					}
+				}
+			}
+			i += 1;
+			List newQ = new ArrayList<Chromosome>(Q);
+			F.add(newQ);
+		}
+		return F;
+	}
+	
+	private int dominates(Chromosome p, Chromosome q) {
+		int counter = 0;
+		for (String objective : objectives) {
+			if (p.getObjectiveValue(objective) < q.getObjectiveValue(objective)) {
+				counter += 1;
+			} else if (p.getObjectiveValue(objective) > q.getObjectiveValue(objective)) {
+				counter -= 1;
+			}
+		}
+		return counter;
+	}
+	
+	private void crowdingDistanceAssignment(List<Chromosome> F) {
 		int l = F.size();
+		double max;
+		double min;
 		for (Chromosome chromosome : F) {
 			chromosome.setCrowdDist(0);
 		}
 		for (String objective : objectives) {
-			sortByObjective(F,objective);
+			sortByObjective(F, objective);
+			F.get(0).setCrowdDist(Double.MAX_VALUE);
+			F.get(l-1).setCrowdDist(Double.MAX_VALUE);
+			max = F.get(l-1).getObjectiveValue(objective);
+			min = F.get(0).getObjectiveValue(objective);
+			for (int i = 1; i < l-1; i++) {
+				F.get(i).addToCrowdDist((F.get(i+1).getObjectiveValue(objective)
+						-F.get(i-1).getObjectiveValue(objective)) / (max-min));
+			}
 		}
 	}
 	
-	private void sortByObjective(ArrayList<Chromosome> F, String objective) {
-		int l = F.size();
-		ArrayList<Chromosome> subA = new ArrayList<Chromosome>(F.subList(0, l/2));
-		ArrayList<Chromosome> subB = new ArrayList<Chromosome>(F.subList(l/2, l));
-		subA.remove(2);
+	private void sortByObjective(List<Chromosome> F, String objective) {
+		if (objective.equals("devi")) {
+			Collections.sort(F, Chromosome.Comparators.DEVI);
+		} else if (objective.equals("edge")) {
+			Collections.sort(F, Chromosome.Comparators.EDGE);
+		} else if (objective.equals("conn")) {
+			Collections.sort(F, Chromosome.Comparators.CONN);
+		}
 	}
 	
 	public static void main(String[] args) {
 		//MasterEA master = new MasterEA("Test_image");
-		ArrayList<Integer> a = new ArrayList<Integer>();
+		List<Integer> a = new ArrayList<Integer>();
 		a.add(2);
 		a.add(5);
 		a.add(19);
