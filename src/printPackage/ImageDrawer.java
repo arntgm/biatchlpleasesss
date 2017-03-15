d1679edb438d2dda1eb93ee9989539a955fdf764
Bendik Witzoe <bendikw@gmail.com>
2017-03-15 13:55:40 +0100

    system now draws image automatically

---
+++ src/printPackage/ImageDrawer.java
@@ -0,0 +1,34 @@
+package printPackage;
+
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class ImageDrawer {	    
+		private ImageDrawer() {}
+	
+	    public static void drawImage(String filename){
+	        BufferedImage img;
+			try {
+				img = ImageIO.read(new File(filename));
+			} catch (IOException e) {
+				e.printStackTrace();
+				return;
+			}
+	        ImageIcon icon=new ImageIcon(img);
+	        JFrame frame=new JFrame();
+	        frame.setLayout(new FlowLayout());
+	        frame.setSize(img.getWidth(), img.getHeight()+100);;
+	        JLabel lbl=new JLabel();
+	        lbl.setIcon(icon);
+	        frame.add(lbl);
+	        frame.setVisible(true);
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    }
+	    
+}
