46fb92f78be47d03479a5f25c0419515f73b54f5
Bendik Witzoe <bendikw@gmail.com>
2017-03-21 18:53:38 +0100

    asda

--- src/genetics/MasterEA.java
+++ src/genetics/MasterEA.java
@@ -239,13 +239,8 @@ public class MasterEA {
 		
 		//create pop using Kmeans
 		List<int[]> pop = new ArrayList<int[]>();
-<<<<<<< HEAD
 		for (int i = 2; i < 12; i++) {
 			for (int j = 0; j < 2; j++) {
-=======
-		for (int i = 4; i < 7; i++) {
-			for (int j = 0; j < 3; j++) {
->>>>>>> refs/remotes/origin/master
 				List<Integer> kmeans = km.getKmeans(i, 20);
 				pop.add(km.getKgenes(kmeans));
 			}
@@ -277,9 +272,7 @@ public class MasterEA {
 //				ImageDrawer.drawImage(pp.generateBufferedImage(oldPopulation.get(0).getSegments(), oldPopulation.get(0).getEdgeMap()));
 			}
 		}
-<<<<<<< HEAD
 		System.out.println("merging....");
-=======
 		//TEST PRINTS
 		ImageDrawer.drawImage(pp.generateBufferedImage(oldPopulation.get(0).getSegments(), oldPopulation.get(0).getEdgeMap()));
 		ImageDrawer.drawImage(pp.generateBufferedBlackAndWhite(oldPopulation.get(0).getSegments(), oldPopulation.get(0).getEdgeMap()));
@@ -292,7 +285,6 @@ public class MasterEA {
 		ImageDrawer.drawImage(pp.generateBufferedImage(oldPopulation.get(4).getSegments(), oldPopulation.get(4).getEdgeMap()));
 		ImageDrawer.drawImage(pp.generateBufferedBlackAndWhite(oldPopulation.get(4).getSegments(), oldPopulation.get(4).getEdgeMap()));
 //		System.out.println("merging....");
->>>>>>> refs/remotes/origin/master
 //		sh.mergeToLimit(oldPopulation.get(0), 15, this.objectives);
 //		ImageDrawer.drawImage(pp.generateBufferedImage(oldPopulation.get(2).getSegments(), oldPopulation.get(2).getEdgeMap()));
 //		ImageDrawer.drawImage(pp.generateBufferedImage(oldPopulation.get(1).getSegments(), oldPopulation.get(1).getEdgeMap()));
@@ -361,17 +353,10 @@ public class MasterEA {
 		String filename = "Test_image_2";
 		String[] objectives = new String[] {"devi", "edge", "conn"};
 
-<<<<<<< HEAD
 		int population = 20;
 		int mstRemoveLimit = 60;
 		int minSegmentSize = 200;
 		int maxGenerations = 0;
-=======
-		int population = 9;
-		int mstRemoveLimit = 60;
-		int minSegmentSize = 200;
-		int maxGenerations = 5;
->>>>>>> refs/remotes/origin/master
 		int tourneySize = 2; //binary
 		double mutateGene = 0;
 		double mutateSeg = 0.1;
